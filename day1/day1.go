afd537388753c559cd19eef4045f82c62ec27e35
Nicholas Novak <nicholasmnovak@gmail.com>
2021-12-03 19:13:30 -0700

    Refactored day 1

--- day1/day1.go
+++ day1/day1.go
@@ -7,59 +7,47 @@ import (
 	"strings"
 )
 
-func part1() {
+func main() {
 	rawInput, err := os.ReadFile("input")
 	if err != nil {
 		panic(err)
 	}
 
 	input := strings.Split(string(rawInput), "\n")
+	input = input[:len(input)-1]
 
+	ints := make([]int, len(input))
+	for ind, item := range input {
+		num, err := strconv.Atoi(item)
+		if err != nil {
+			panic(err)
+		}
+		ints[ind] = num
+	}
+
+	part1(ints)
+	part2(ints)
+}
+
+func part1(input []int) {
 	var numPrev int
 
-	var prev int
-	for _, depth := range input {
-		num, _ := strconv.Atoi(depth)
-		if num > prev {
+	for ind, num := range input {
+		if ind > 0 && num > input[ind-1] {
 			numPrev++
 		}
-		prev = num
 	}
 
 	fmt.Println(numPrev)
 }
 
-func part2() {
-	rawInput, err := os.ReadFile("input")
-	if err != nil {
-		panic(err)
-	}
-
-	strInput := strings.Split(string(rawInput), "\n")
-
-	input := make([]int, len(strInput))
-	for ind, val := range strInput {
-		if val == "" {
-			continue
-		}
-		num, err := strconv.Atoi(val)
-		if err != nil {
-			panic(err)
-		}
-		input[ind] = num
-	}
-
+func part2(input []int) {
 	var numPrev int
 
-	var prev int
-	for ind, _ := range input {
-		if ind-2 < 0 {
-			continue
-		}
-		if sum(input[ind-2:ind+1]) > prev {
+	for ind := range input {
+		if ind > 2 && sum(input[ind-2:ind+1]) > sum(input[ind-3:ind]) {
 			numPrev++
 		}
-		prev = sum(input[ind-2 : ind+1])
 	}
 
 	fmt.Println(numPrev)
@@ -71,9 +59,4 @@ func sum(nums []int) int {
 		total += num
 	}
 	return total
-}
-
-func main() {
-	//part1()
-	part2()
 }
